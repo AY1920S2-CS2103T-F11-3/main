ea799910baacd5b1022701bac9510ed8bbbf8192
Leslie Harland <leslieharland@outlook.com>
2020-03-15 17:21:48 +0800

    Able to set and view favorites

--- src/test/java/seedu/foodiebot/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/foodiebot/logic/commands/AddCommandTest.java
@@ -208,6 +208,21 @@ public class AddCommandTest {
         }
 
         @Override
+        public void setFavorite(Food food) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Food> getFilteredFavoriteFoodList() {
+            return null;
+        }
+
+        @Override
+        public void updateFilteredFavoriteList(Predicate<Food> predicateShowAll) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public ObservableList<Food> getFilteredFoodList() {
             throw new AssertionError("This method is a placeholder");
         }
@@ -219,7 +234,7 @@ public class AddCommandTest {
 
         @Override
         public void updateFilteredFoodList(Predicate<Food> predicate) {
-
+            throw new AssertionError("This method should not be called.");
         }
     }
 
